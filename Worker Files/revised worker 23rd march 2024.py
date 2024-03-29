d293d3fbc0222fdba53272503d5d730dbf4017e6
Chris <60389799+chrisgwx@users.noreply.github.com>
2024-03-29 19:44:26 -0400

    Update revised worker 29rd march 2024.py
    Updated to handle new namespace mech

--- Worker Files/revised worker 23rd march 2024.py
+++ Worker Files/revised worker 23rd march 2024.py
@@ -4,33 +4,42 @@ import time
 import tkinter as tk
 import threading
 import uuid
+from threading import Lock
 
-# Define the server URL
-server_url = 'https://webservice-0-2.onrender.com'
+# Constants
+SERVER_URL = 'https://webservice-0-2.onrender.com'
+NAMESPACE = '/ccdev'
+
+# Global variables
+running = False
+running_lock = Lock()
 
 sio = socketio.Client()
 
-@sio.event
+
+@sio.event(namespace=NAMESPACE)
 def connect():
     print('Connected to server')
 
-@sio.event
+
+@sio.event(namespace=NAMESPACE)
 def disconnect():
     print('Disconnected from server')
 
-    # You can attempt reconnecting here or notify the user
 
-@sio.event
+@sio.event(namespace=NAMESPACE)
 def connection_success(data):
     print('Connection successful:', data)
 
-@sio.event
+
+@sio.event(namespace=NAMESPACE)
 def data_inserted(data):
     print('New item inserted:', data)
 
-# Data model for the client
+
 def generate_random_data():
     sysUUID = str(uuid.uuid4())
+    lastrowid = random.randint(1, 100)
     sid = random.randint(1, 100)
     score = random.uniform(0, 10)
     timestamp = int(time.time())
@@ -39,6 +48,7 @@ def generate_random_data():
 
     json_data = {
         "sysUUID": sysUUID,
+        "lastrowid": lastrowid,
         "sid": sid,
         "score": score,
         "timestamp": timestamp,
@@ -46,30 +56,38 @@ def generate_random_data():
         "ts": ts
     }
 
-    sio.emit('data_received', json_data)
+    sio.emit('data_received', json_data, namespace='/ccdev')
     print(f"Sent data to server: {json_data}")
+
 
 def start_sending_data():
     global running
-    running = True
+    with running_lock:
+        running = True
     while running:
         generate_random_data()
         time.sleep(3)
 
+
 def stop_sending_data():
     global running
-    running = False
+    with running_lock:
+        running = False
     enable_start_button()
+
 
 def disable_start_button():
     start_button.config(state=tk.DISABLED)
 
+
 def enable_start_button():
     start_button.config(state=tk.NORMAL)
+
 
 def start_button_clicked():
     disable_start_button()
     threading.Thread(target=start_sending_data).start()
+
 
 root = tk.Tk()
 root.title("Tester App")
@@ -81,6 +99,9 @@ start_button.pack()
 stop_button = tk.Button(root, text="Stop Sending Data", command=stop_sending_data)
 stop_button.pack()
 
-sio.connect(server_url)
+try:
+    sio.connect(SERVER_URL, namespaces=[NAMESPACE], transports=['websocket'])
+except socketio.exceptions.ConnectionError as e:
+    print(f"Failed to connect: {str(e)}")
 
 root.mainloop()
