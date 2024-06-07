0d869658f96ec08c7efe9fadd7631627eecdd3c2
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-07 17:57:08 +0000

    bare example population for simulation data

--- service.js
+++ service.js
@@ -2,8 +2,9 @@ const create_server=require('./create_server.js'), {get_user_boxes,all}=require(
 const {serve}=require('webject'), {auth_head,auth_value,admin_route,admin_token}=process.env //as for now, admin route is unused
 let webject=null, fs=require('node:fs'), html=fs.readFileSync('iframe.html'), admin=fs.readFileSync('admin.html')
 const server=create_server(async function(req,res){
-  if(webject.authTokens[req.url.substring(1)]) return res.end(html); //for one user
-  if(req.headers.url.substring(1)===admin_route) return res.end(admin); //for admin
+  res.setHeader('Content-Type','text/html')
+  if(webject.authTokens[decodeURIComponent(req.url.substring(1))]) return res.end(html); //for one user
+  if(req.url.substring(1)===admin_route) return res.end(admin); //for admin
   if(req.headers[auth_head]!==auth_value) return res.end(""); //authentication barrier for creating new tokens
   const boxes=await get_user_boxes(req.headers['mcylia-user'])
   return res.end(webject.addToken(1,boxes)) //so to access the link, it'd be webservice_site/what_was_returnedhere
--- storage.js
+++ storage.js
@@ -1,11 +1,13 @@
 //is a simulation right now but will handle communication with the database
-const simulation_boxes={}, simulation_users={}, all={simulation_boxes,simulation_users}
+const simulation_boxes={mcylia_abcd:{online:false}}
+const simulation_users={undefined: {metadata:"whocares",boxes:{mcylia_abcd:simulation_boxes.mcylia_abcd}} }
+const all={simulation_boxes,simulation_users}
 setInterval(function simulate_changes(){
-  let id=Math.floor(  Math.random()*Object.keys(simulate_data).length  )
+  let keys=Object.keys(simulation_boxes), id=keys[ Math.floor(Math.random()*keys.length) ]
   let box=simulation_boxes[id]
   if(Math.random()<0.5) box.online=!box.online; //flip online presence, 50% chance
 },2e3)
 function get_user_boxes(user_id){
-  return simulation_users[user_id]
+  return simulation_users[user_id].boxes
 }
 module.exports={get_user_boxes,all}
