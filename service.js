4e6bf88a71d4a3a1159df0188a66a2dca6471b58
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-07 17:35:27 +0000

    realtime data serving logic defined the admin route is for seeing all boxes and the other routes are for when the other domain with auth headers requests to get a url that will show boxes that ONLY THAT USER OWNS

--- service.js
+++ service.js
@@ -1,5 +1,20 @@
-const {serve}=require('webject'), create_server=require('./create_server.js'), get_boxes=require('./storage.js')
-const {authorisation}=process.env
-const server=create_server(function(req,res){
-  
+const create_server=require('./create_server.js'), {get_user_boxes,all}=require('./storage.js')
+const {serve}=require('webject'), {auth_head,auth_value,admin_route,admin_token}=process.env //as for now, admin route is unused
+let webject=null, fs=require('node:fs'), html=fs.readFileSync('iframe.html'), admin=fs.readFileSync('admin.html')
+const server=create_server(async function(req,res){
+  if(webject.authTokens[req.url.substring(1)]) return res.end(html); //for one user
+  if(req.headers.url.substring(1)===admin_route) return res.end(admin); //for admin
+  if(req.headers[auth_head]!==auth_value) return res.end(""); //authentication barrier for creating new tokens
+  const boxes=await get_user_boxes(req.headers['mcylia-user'])
+  return res.end(webject.addToken(1,boxes)) //so to access the link, it'd be webservice_site/what_was_returnedhere
 },8080)
+webject=serve(all,server)
+webject.addToken(1,all,admin_token) //admin token to see all boxes
+webject.addListener("connect",function(ev){
+  clearTimeout(ev.token._inactive)
+  ev.lock()
+})
+webject.addListener("disconnect",function(ev){
+  ev.token._inactive=setTimeout(_=>webject.endToken(ev.token.authToken),2e4) //after 20 seconds, token is revoked
+  ev.unlock()
+})
--- storage.js
+++ storage.js
@@ -1,13 +1,11 @@
 //is a simulation right now but will handle communication with the database
-const simulation_data = {}
+const simulation_boxes={}, simulation_users={}, all={simulation_boxes,simulation_users}
 setInterval(function simulate_changes(){
   let id=Math.floor(  Math.random()*Object.keys(simulate_data).length  )
-  let box=simulation_data[id]
+  let box=simulation_boxes[id]
   if(Math.random()<0.5) box.online=!box.online; //flip online presence, 50% chance
 },2e3)
-function get_boxes(data){
-  //data is the header value
-  const mcylia_boxes = data.split(';').map(id=>simulation_data[id])
-  return mcylia_boxes
+function get_user_boxes(user_id){
+  return simulation_users[user_id]
 }
-module.exports=get_boxes
+module.exports={get_user_boxes,all}
