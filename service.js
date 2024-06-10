de6818c00f8accb5cb30587e7f48971caf783227
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-10 12:18:43 +0000

    bare bones demo ready

--- service.js
+++ service.js
@@ -1,16 +1,18 @@
 const create_server=require('./create_server.js'), {get_user_boxes,all}=require('./storage.js')
 const {serve}=require('webject'), {auth_head,auth_value,admin_route,admin_token}=process.env //as for now, admin route is unused
 let webject=null, fs=require('node:fs'), html=fs.readFileSync('iframe.html'), admin=fs.readFileSync('admin.html')
+
 const server=create_server(async function(req,res){
   res.setHeader('Content-Type','text/html')
   if(webject.authTokens[decodeURIComponent(req.url.substring(1))]) return res.end(html); //for one user
-  if(req.url.substring(1)===admin_route) return res.end(admin); //for admin
+  if(req.url.substring(1)===(admin_route||"admin")) return res.end(admin); //for admin -------------
   if(req.headers[auth_head]!==auth_value) return res.end(""); //authentication barrier for creating new tokens
   const boxes=await get_user_boxes(req.headers['mcylia-user'])
   return res.end(webject.addToken(1,boxes)) //so to access the link, it'd be webservice_site/what_was_returnedhere
 },8080)
+
 webject=serve(all,server)
-webject.addToken(1,all,admin_token) //admin token to see all boxes
+webject.addToken(1,all,(admin_token||"password")) //admin token to see all boxes ---------------
 webject.addListener("connect",function(ev){
   clearTimeout(ev.token._inactive)
   ev.lock()
