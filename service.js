64a68e228ec6875b4a648557b163fd5a0d28ec1c
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-20 12:04:46 +0000

    considering ngrok

--- service.js
+++ service.js
@@ -4,7 +4,7 @@ let webject=null, fs=require('node:fs'), html=fs.readFileSync('iframe.html')
 
 const server=create_server(async function(req,res){
   res.setHeader('Content-Type','text/html')
-  if(webject.authTokens[decodeURIComponent(req.url.substring(1))]) return res.end(html); //for one user
+  if(webject.authTokens.get(decodeURIComponent(req.url.substring(1)))) return res.end(html); //for one user
   if(req.headers[AUTH_HEAD]!==AUTH_VALUE) return res.end(""); //authentication barrier for creating new tokens
   const boxes=await get_user_boxes(req.headers['mcylia-box'])
   const token=webject.addToken(1,boxes)
@@ -23,3 +23,8 @@ webject.addListener("disconnect",function(ev){
   //after 5 seconds without connecting, token is revoked
   ev.unlock()
 })
+
+/*//ngrok block start
+const ngrok=require('ngrok');
+ngrok.connect(8080).then(console.log);
+//ngrok block stop*/
