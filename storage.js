5d7c3072d62808003c4df66c6905c5f7ff654ab9
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-25 02:59:49 +0000

    browser script for webject forgotten and rfid changed queued

--- storage.js
+++ storage.js
@@ -7,6 +7,9 @@ const deviceEventSummaryLogs=DB_CLIENT.createConnection({...db_opts,database:'de
 deviceEventSummaryLogs.connect() //eventIndex,sysUUID,song_id,score,timestamp,duration,delta
 //deviceEventSummaryLogs also has a songGroups table which seems to be aggregated data from the rest of tables but less attributes per record
 //songGroups has sg_index,summary_eventIndex,songID,sysUUID
+//const devices=DB_CLIENT.createConnection({...db_opts,database:'devices'}) //rfidActivity: activity_id,UUID(unused),sysUUID,clientId(unused),state,timestamp
+//devices.connect()
+//third db queued
 
 async function query(q,db){
   let resolve=null, p=new Promise(r=>resolve=r)
@@ -16,13 +19,19 @@ async function query(q,db){
 function queryString(id){
   return 'select * from `'+id+'` where eventIndex=(select MAX(eventIndex) from `'+id+'`);'
 }
+function rfidQuery(id){
+  return `select state, timestamp from rfidActivity where sysUUID = '${id}' and activity_id =
+  (select MAX(activity_id) from devices.rfidActivity where sysUUID = '${id}');`
+}
 async function loadBoxes(){
   (await query('show tables',deviceEventLogs))
   .map(obj=>Object.values(obj)[0]).filter(text=>text.startsWith('mCylia'))
   .forEach(async function(id){
     boxes[id]={
       events:await query(queryString(id),deviceEventLogs),
-      summaries:await query(queryString(id),deviceEventSummaryLogs)
+      summaries:await query(queryString(id),deviceEventSummaryLogs),
+      //rfid:(await query(rfidQuery(id),devices))[0] //state: "powered on" or "waiting"
+      //rfid object queued
     }
   })
 }
