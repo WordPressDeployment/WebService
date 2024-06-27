aff67f3b826648d8a230033ff897b796972601c8
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-06-27 10:31:01 -0400

    storage service renamed

--- service.js
+++ service.js
@@ -1,4 +1,4 @@
-const create_server=require('./create_server.js'), get_user_boxes=require('./storage.js')
+const create_server=require('./create_server.js'), get_box_info=require('./storage.js')
 const {serve}=require('webject'), {AUTH_HEAD,AUTH_VALUE}=process.env //as for now, admin route is unused
 let webject=null, fs=require('node:fs'), html=fs.readFileSync('iframe.html')
 function ATOB(data){
@@ -16,7 +16,7 @@ const server=create_server(async function(req,res){
   if(req.headers[AUTH_HEAD]!==AUTH_VALUE) return res.end(""); //authentication barrier for creating new tokens
   req.headers['mcylia-box']||='mCylia-M4-x0080_cylia868' //default mcylia-box header
   req.headers['start-and-end']||='1718211343452;1719420976624' //default start-and-end header
-  const boxes=await get_user_boxes(req.headers['mcylia-box'])
+  const boxes=await get_box_info(req.headers['mcylia-box'],req.headers['start-and-end'])
   const token=webject.addToken(1,boxes)
   webject.authTokens.get(token)._inactive=setTimeout(_=>webject.endToken(token),1e4)
   //after 10 seconds without connecting, token is revoked
