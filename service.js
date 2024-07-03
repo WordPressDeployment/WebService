9099de6ca7803eb1972290d9684b83a22e58dc37
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-07-03 10:37:14 -0400

    default headers commented out
    The headers MUST be given

--- service.js
+++ service.js
@@ -14,8 +14,8 @@ const server=create_server(async function(req,res){
     return res.end(html); //for one user
   }
   if(req.headers[AUTH_HEAD]!==AUTH_VALUE) return res.end(""); //authentication barrier for creating new tokens
-  req.headers['mcylia-box']||='mCylia-M4-x0080_cylia868' //default mcylia-box header
-  req.headers['start-and-end']||='1707797694892;1707970494892' //default start-and-end header
+  //req.headers['mcylia-box']||='mCylia-M4-x0080_cylia868' //default mcylia-box header
+  //req.headers['start-and-end']||='1707797694892;1707970494892' //default start-and-end header
   const boxes=await get_box_info(req.headers['mcylia-box'],req.headers['start-and-end'])
   const token=webject.addToken(1,boxes)
   webject.authTokens.get(token)._inactive=setTimeout(_=>webject.endToken(token),1e4)
