7cb56f453d33b886da6095244f8c0afe796a6b21
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-07-26 15:59:40 +0000

    more logging yet again

--- storage.js
+++ storage.js
@@ -92,10 +92,10 @@ async function get_box_info(box_id,time_range){
   if(record) return record;
   record=await update({},key)
   cache.set(key,record)
-  console.log('made it here')
   return record
 }
 async function get_state_info(header){
+  console.log(header)
   if(state_headers.has(header)) return state_headers.get(header);
   const ids=header.split(';'), state_info=Array(ids.length);
   if(ids.some(header=>!header.startsWith('mCylia-'))) return {state_info}; //sysUUID validation
