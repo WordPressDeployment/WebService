fcfe5e8efcfa3b52ff82edb74b0809c9f6d792d5
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2024-07-29 13:37:12 -0400

    optimisations to queries and mysql connection

--- storage.js
+++ storage.js
@@ -1,60 +1,47 @@
 const {DB_URL,DB_USER,DB_PASS}=process.env, DB_CLIENT=require('mysql')
 const db_opts={host:DB_URL,user:DB_USER,password:DB_PASS}, cache=new Map(), states=new Map(), state_headers=new Map()
 //cylia.songs_v18
-//song_id,title,artist,album,track_no,length,date,genre         | varchar(35)           | YES  |     | NULL    |                |
-/*| isrc          | varchar(40)           | YES  |     | NULL    |                |
-| rejected      | tinyint(4)            | YES  |     | 0       |                |
-| fingerprinted | tinyint(4)            | YES  |     | 0       |                |
-| file_sha1     | binary(20)            | NO   | UNI | NULL    |                |
-| song_feats    | mediumint(8) unsigned | YES  |     | 0       |                |
-| feats_logged  | mediumint(8) unsigned | YES  |     | 0       |                |
-| max_dB*/
+//song_id,title,artist,album,track_no,length,date,genre,isrc,rejected,fingerprinted,file_sha1,song_feats,feats_logged,max_dB
 
-let deviceEventLogs=DB_CLIENT.createConnection({...db_opts,database:'deviceEventLogs'})
-deviceEventLogs.connect() //eventIndex,sourceTimestamp,recogTimestamp,sourceIndex,song_id,tm,tc,fm,fc,fMSE,tMSE,tinliers,finliers,samplePeriod,score,db,version,isNew
-deviceEventLogs.on('error',function(){
-  deviceEventLogs=DB_CLIENT.createConnection({...db_opts,database:'deviceEventLogs'})
-  deviceEventLogs.connect()
-  deviceEventLogs.on('error',arguments.callee)
-})
+//deviceEventLogs.[some_sysUUID]
+//eventIndex,sourceTimestamp,recogTimestamp,sourceIndex,song_id,tm,tc,fm,fc,fMSE,tMSE,tinliers,finliers,samplePeriod,score,db,version,isNew
 
-let deviceEventSummaryLogs=DB_CLIENT.createConnection({...db_opts,database:'deviceEventSummaryLogs'})
-deviceEventSummaryLogs.connect() //eventIndex,sysUUID,song_id,score,timestamp,duration,delta
+//deviceEventSummaryLogs.[some_sysUUID]
+//eventIndex,sysUUID,song_id,score,timestamp,duration,delta
 //deviceEventSummaryLogs also has a songGroups table which seems to be aggregated data from the rest of tables but less attributes per record
 //songGroups has sg_index,summary_eventIndex,songID,sysUUID
-deviceEventSummaryLogs.on('error',function(){
-  deviceEventSummaryLogs=DB_CLIENT.createConnection({...db_opts,database:'deviceEventSummaryLogs'})
-  deviceEventSummaryLogs.connect()
-  deviceEventSummaryLogs.on('error',arguments.callee)
+
+let connection=DB_CLIENT.createConnection(db_opts)
+connection.connect()
+connection.on('error',function(){
+  connection=DB_CLIENT.createConnection(db_opts)
+  connection.connect()
+  connection.on('error',arguments.callee)
 })
 
-let devices=DB_CLIENT.createConnection({...db_opts,database:'devices'}) //stateActivity: activity_id,sysUUID,clientId(unused),GPSstate,mCyliaHeartbeat,sourceTimestamp,hum,audioGood,powerMeas
-devices.connect()
-devices.on('error',function(){
-  devices=DB_CLIENT.createConnection({...db_opts,database:'devices'})
-  devices.connect()
-  devices.on('error',arguments.callee)
-})
-
-
-async function query(q,db){
+async function query(q){
   let resolve=null, p=new Promise(r=>resolve=r)
-  db.query(q,function(err,result){if(err)console.log(err);resolve(err?[]:result)}) //soft error handling because yes
+  connection.query(q,function(err,result){
+    if(err)  console.log(err);
+    resolve(err? []: result)
+  }) //soft error handling because yes
   return await p
 }
 function eventQueryString(id,start,end){
-  let sql_start=new Date(start).toISOString().replace('T', ' ').replace('Z', '')
+  let ev_start=new Date(start).toISOString().replace('T', ' ').replace('Z', '')
+  let sql_start=`SELECT MAX(timestamp) from deviceEventSummaryLogs.\`${id}\` where timestamp <= '${ev_start}'`
   let sql_end=new Date(end).toISOString().replace('T', ' ').replace('Z', '')
   //return `select * from \`${id}\` where sourceTimestamp between '${sql_start}' and '${sql_end}';`
-  return `SELECT ds.eventIndex as eventIndex, ds.song_id as song_id, ds.sourceIndex as sourceIndex, ds.sourceTimestamp as sourceTimestamp, s.title, s.artist, s.album, s.genre, (tinliers/6*finliers/50) as score from deviceEventLogs.\`${id}\` as ds JOIN cylia.songs_v18 as s on s.song_id=ds.song_id ORDER BY ds.sourceTimestamp DESC limit 10;`
+  return `SELECT ds.eventIndex as eventIndex, ds.song_id as song_id, ds.sourceIndex as sourceIndex, ds.sourceTimestamp as sourceTimestamp, s.title, s.artist, s.album, s.genre, (tinliers/6*finliers/50) as score FROM deviceEventLogs.\`${id}\` WHERE sourceTimestamp BETWEEN (${sql_start}) and '${sql_end}' as ds JOIN cylia.songs_v18 as s on s.song_id=ds.song_id ORDER BY ds.sourceTimestamp DESC;`
 }
 function summaryQueryString(id,start,end){
   let sql_start=new Date(start).toISOString().replace('T', ' ').replace('Z', '')
   let sql_end=new Date(end).toISOString().replace('T', ' ').replace('Z', '')
-  return `select * from \`${id}\` where timestamp between '${sql_start}' and '${sql_end}';`
+  //return `select * from \`${id}\` where timestamp between '${sql_start}' and '${sql_end}';`
+  return `SELECT ds.eventIndex as eventIndex, ds.sysUUID as sysUUID, ds.song_id as song_id, ds.score as score, ds.timestamp as timestamp, (ds.duration*ds.delta) as seconds FROM deviceEventSummaryLogs.\`${id}\` WHERE  timestamp BETWEEN '${sql_start}' and '${sql_end}' as ds JOIN cylia.songs_v18 as s on s.song_id=ds.song_id ORDER BY ds.timestamp DESC;`
 }
 function stateQuery(id){
-  return `select * from stateActivity where sourceTimestamp >= now(6) - interval 5 minute and sysUUID='${id}';`
+  return `select * from devices.stateActivity where sourceTimestamp >= now(6) - interval 5 minute and sysUUID='${id}';`
 }
 function parseTimes(time_range){
   if(!time_range) return [0,0];
@@ -65,26 +52,26 @@ function parseTimes(time_range){
 async function update(record,key){
   const [box_id,time_range]=JSON.parse(key)
   const [start,end]=parseTimes(time_range)
-  record.events=await query(eventQueryString(box_id,start,end),deviceEventLogs)
-  record.summaries=await query(summaryQueryString(box_id,start,end),deviceEventSummaryLogs)
-  let maxLength=Math.max(record.events.length,record.summaries.length)
-  //convert date values to long ints start
-  for(let i=0;i<maxLength;i++){
-    if(i<record.events.length){
-      if(typeof record.events[i].sourceTimestamp!=="number") record.events[i].sourceTimestamp-=0;
-      if(typeof record.events[i].recogTimestamp!=="number")record.events[i].recogTimestamp-=0;
-    }
-    if(i<record.summaries.length)
-      if(typeof record.summaries[i].timestamp!=="number") record.summaries[i].timestamp-=0;
+  const events=await query(eventQueryString(box_id,start,end))
+  const summaries=await query(summaryQueryString(box_id,start,end))
+  record.events ||= [];
+  record.summaries ||= [];
+  for(let i=record.events.length;i<events.length){
+    record.events[i] = events[i];
+    record.events[i].sourceTimestamp -= 0; //convert date value to long int
+    record.events[i].recogTimestamp -= 0; //convert date value to long int
   }
-  //convert date values to long ints stop
+  for(let i=0;i<record.summaries.length;i++){
+    record.summaries[i] = summaries[i];
+    record.summaries[i].timestamp -= 0; //convert date value to long int
+  }
   if(end<=Date.now()) record.state={offline:true}; //claim it is offline if the event isn't current
-  else record.state=(await query(stateQuery(box_id),devices))[0] || {offline:true}; //query exists or assumed offline
+  else record.state=(await query(stateQuery(box_id)))[0] || {offline:true}; //query exists or assumed offline
   return record
 }
 async function update_states(record,id){
   if(id.includes(';')) return null; //do nothing (not a specific id)
-  let temp=await query(stateQuery(id),devices)
+  let temp=await query(stateQuery(id))
   for(let i=0;i<temp.length;i++) record[i]=temp[i];
   record.length=temp.length;
   return record;
